b46f6f274063266749eba45b78f71d309ff73069
GNYAR <s901121s901121@gmail.com>
2021-07-25 00:08:01 +0800

    Use components

--- src/components/Organize/officer/index.js
+++ src/components/Organize/officer/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
-import { Row, Col, Card, ButtonGroup, Button } from 'react-bootstrap'
-import { XCircle, PencilSquare, PersonPlusFill } from 'react-bootstrap-icons';
-import { OfficeModal } from './modal';
+import { Authority } from './Authority';
+import { OfficerList } from './OfficerList';
 
 const officers = {
   會長: "1110634001",
@@ -12,59 +11,19 @@ const officers = {
   器材長: "1110634004",
   生活長: "1110634008",
   資訊長: "1110634007"
+};
+const authorities = {
+  組織負責人: ["會長", "副會長"],
+  財務負責人: ["財務長", "副會長"],
+  會議記錄人: ["資訊長", "副會長"],
 }
 
+
 export const Officer = () => {
-  const [AddShow, setAddShow] = React.useState(false);
-  const [position, setPosition] = React.useState();
-  const [EditShow, setEditShow] = React.useState(false);
-  const [DeleteShow, setDeleteShow] = React.useState(false);
   return (
     <>
-      <Row>
-        <Col xs={{ span: 4, offset: 8 }} className="py-1">
-          <Button
-            variant="info"
-            className="float-right"
-            onClick={() => setAddShow(true)}
-          >
-            <PersonPlusFill size="20" className="mr-2" />新增
-          </Button>
-        </Col>
-        {Object.keys(officers).map(x => (
-          <Col className="py-1" sm="6" lg="4">
-            <Card body bg="dark" className="text-white">
-              <ButtonGroup className="float-right">
-                <Button
-                  variant="dark"
-                  className="px-2 pt-0 pb-1"
-                  onClick={() => {
-                    setPosition(x);
-                    setEditShow(true);
-                  }}
-                >
-                  <PencilSquare size="20" />
-                </Button>
-                <Button
-                  variant="dark"
-                  className="px-2 pt-0 pb-1"
-                  onClick={() => {
-                    setPosition(x);
-                    setDeleteShow(true);
-                  }}
-                >
-                  <XCircle size="20" />
-                </Button>
-              </ButtonGroup>
-              <Card.Title>{x}</Card.Title>
-              <Card.Subtitle className="text-info">{officers[x]}</Card.Subtitle>
-            </Card>
-          </Col>
-        ))}
-      </Row>
-      <OfficeModal.ADD show={AddShow} onHide={() => setAddShow(false)} />
-      <OfficeModal.EDIT position={position} show={EditShow} onHide={() => setEditShow(false)} />
-      <OfficeModal.DELETE position={position} show={DeleteShow} onHide={() => setDeleteShow(false)} />
+      <OfficerList officers={officers} />
+      <Authority authorities={authorities} officers={officers} />
     </>
   )
 }
