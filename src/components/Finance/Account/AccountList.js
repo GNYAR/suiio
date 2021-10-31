9eaf00c5a80a242d50107a9318f59da663e0b4e6
GNYAR <s901121s901121@gmail.com>
2021-10-31 12:18:51 +0800

    Add page button in account list

--- src/components/Finance/Account/AccountList.js
+++ src/components/Finance/Account/AccountList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Row, Col, Card, Button } from 'react-bootstrap'
+import { Row, Col, Card, Button, ButtonToolbar, ButtonGroup, Dropdown, DropdownButton } from 'react-bootstrap'
 import { Pen } from 'react-bootstrap-icons'
 import { ModalContent } from './ModalContent'
 import { ModalAdd } from './ModalAdd'
@@ -9,11 +9,12 @@ export class AccountList extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            accounts: [],
             selected: {},
             ContentShow: false,
             AddShow: false,
-            review: false
+            review: false,
+            pages: [],
+            pagenumber: 0,
         }
         this.update()
     }
@@ -21,7 +22,21 @@ export class AccountList extends Component {
     update = () => {
         fetch('http://suiio.nutc.edu.tw:2541/api/account/fetch/all')
             .then((res) => res.json())
-            .then((data) => this.setState({ accounts: data }))
+            .then((data) => {
+                let cnt = 0
+                const pages = data.reduce((arr, v, k) => {
+                    const n = k % 12
+                    if(n) {
+                        arr[cnt][n] = v
+                    } else {
+                        cnt++
+                        arr = [...arr, [v]]
+                    }
+                    return arr
+                }, [[]])
+                pages.shift()
+                this.setState({pages})
+            })
     }
 
     onHide = (show) => {
@@ -39,18 +54,39 @@ export class AccountList extends Component {
         return (
             <>
                 <Row noGutters>
-                    <Col xs="12" className="py-3">
-                        <Button
-                            variant="info"
-                            className="float-right"
-                            onClick={() => this.setState({ AddShow: true })}
-                        >
-                            <Pen size="20" className="mr-2" />
-                            提交申請
-                        </Button>
+                    <Col xs="12" className="py-2">
+                        <ButtonToolbar className="justify-content-between">
+                            <ButtonGroup className="mr-2" aria-label="First group">
+                                <Button variant="secondary" onClick={() => {
+                                    if(this.state.pagenumber)
+                                        this.setState({ pagenumber: this.state.pagenumber-1})
+                                }}>&lt;</Button>
+                                <DropdownButton as={ButtonGroup} title={this.state.pagenumber+1} variant="secondary">
+                                        {this.state.pages.map((v, i) => {
+                                            return (
+                                                <Dropdown.Item
+                                                    active = { this.state.pagenumber === i }
+                                                    onClick={() => this.setState({ pagenumber: i })}
+                                                >{i+1}</Dropdown.Item>
+                                            )
+                                        })}
+                                </DropdownButton>
+                                <Button variant="secondary" onClick={() => {
+                                    if(this.state.pagenumber < this.state.pages.length-1)
+                                        this.setState({ pagenumber: this.state.pagenumber+1})
+                                }}>&gt;</Button>
+                            </ButtonGroup>
+                            <Button
+                                variant="info"
+                                onClick={() => this.setState({ AddShow: true })}
+                            >
+                                <Pen size="20" className="mr-2" />
+                                提交申請
+                            </Button>
+                        </ButtonToolbar>
                     </Col>
-                    {this.state.accounts?.result ??
-                    this.state.accounts?.map(x => (
+                    {this.state.pages.length ?
+                    this.state.pages[this.state.pagenumber].map(x => (
                         <Col className="px-1 py-2" md="6" lg="4">
                             <Card
                                 bg="dark"
@@ -78,7 +114,7 @@ export class AccountList extends Component {
                                 </Card.Body>
                             </Card>
                         </Col>
-                    ))}
+                    )) : "No Data"}
                 </Row>
                 <ModalContent
                     show={this.state.ContentShow}
